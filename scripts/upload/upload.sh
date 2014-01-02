e5cf235f2ac38cfdc2aa7d104ba2ad2be1c2ae4e
Ryan Szeto <rszeto@student.umass.edu>
2014-01-01 19:25:03 -0500

    Uploading now requires explicit destination (specified in uploadConfig.txt)

--- scripts/upload/upload.sh
+++ scripts/upload/upload.sh
@@ -2,6 +2,7 @@
 
 user=
 host=
+rmt_upload=
 
 config=/home/paol/paol-code/uploadConfig.txt
 while read a b; do
@@ -9,13 +10,17 @@ while read a b; do
 		user=$b
 	elif [ "$a" = "host:" ]; then
 		host=$b
+	elif [ "$a" = "rmt_upload:" ]; then
+		rmt_upload=$b
 	fi
 done < $config
 
 echo "User: $user"
 echo "Host: $host"
+echo "Remote upload folder: $rmt_upload"
 
-if [ -z "$user" ] || [ -z "$host" ]; then
+# Exit if one of the parameters is not specified
+if [ -z "$user" ] || [ -z "$host" ] || [ -z "$rmt_upload" ]; then
 	echo "Upload configuration file has the wrong format"
 	exit 1
 fi
@@ -25,29 +30,21 @@ sem=$(basename $(dirname $(dirname $outDir)))
 class=$(basename $(dirname $outDir))
 
 uploaded=/home/paol/recordings/uploaded
-sshid=/home/paol/.ssh/id_rsa
-mediaDirTag=$(curl -k https://$host/api/config | grep mediaDirectory)
-#trim {"mediaDirectory":"/ and "/}
-mediaDirTag=${mediaDirTag:20:${#mediaDirTag}-22}
-echo $mediaDirTag
-rmt_upload_fldr=/data1/www/$mediaDirTag
-echo $rmt_upload_fldr
 
 #upload files
+sshid=/home/paol/.ssh/id_rsa
 ssh -i $sshid $user@$host touch /var/lock/manic.lck
-ssh -i $sshid $user@$host mkdir -p $rmt_upload_fldr/$sem/$class
-rsync -avz -e "ssh -i $sshid" $outDir $user@$host:$rmt_upload_fldr/$sem/$class
+ssh -i $sshid $user@$host mkdir -p $rmt_upload/$sem/$class
+rsync -avz -e "ssh -i $sshid" $outDir $user@$host:$rmt_upload/$sem/$class
 STATUS=$?
 ssh -i $sshid $user@$host rm /var/lock/manic.lck
 
 if [ $STATUS = 0 ]; then
   echo "Uploaded lecture $(basename $outDir) from $class in $sem, moving to uploaded folder"
   mkdir -p $uploaded/$sem/$class
-	#touch local lock?
   mv $outDir $uploaded/$sem/$class
-  #remove local lock?
 else
-  echo "rsync failure"    
+  echo "rsync failure"
 fi
 
 exit $STATUS
