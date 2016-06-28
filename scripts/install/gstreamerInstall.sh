59ab24b6be038a260fc011430deea3a96934193e
tfcabral <fari7153@kettering.edu>
2016-06-28 11:41:53 -0400

    added c920 setup

--- scripts/install/gstreamerInstall.sh
+++ scripts/install/gstreamerInstall.sh
@@ -11,3 +11,9 @@ sudo apt-get install v4l-utils
 #Install gstreamer package and plugins
 
 sudo apt-get install gstreamer1.0*
+sudo apt-get install gstreamer-tools
+
+#Configure C920 Cameras into H264 at specific resolution and framerate
+
+v4l2-ctl --set-fmt-video=width=800,height=448,pixelformat=1
+v4l2-ctl --set-parm=24
