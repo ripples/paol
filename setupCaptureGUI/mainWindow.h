ac2b1f35b72e08f51cd6de240b1ba49a16626587
tfcabral <tiagofcabralz@gmail.com>
2017-11-17 09:57:59 -0500

    fix

--- setupCaptureGUI/mainWindow.h
+++ setupCaptureGUI/mainWindow.h
@@ -51,6 +51,7 @@ public:
     // GENERAL VARIABLES
     vector< vector<string> >usbVideo; //vector to store USB video info
     vector< vector<string> >audioNumber; //vector to store audio info
+    std::string alsaString;
     int camCount; //Amount of connected cameras
     int whiteboards; //Amount of selected whiteboards
     int captureDevices; // Devices that are either WBs or VGA2USB
