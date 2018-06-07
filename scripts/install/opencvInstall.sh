7edf5af50f30509ca7041c1e19dedc47706ef7c2
paul <drpauled@gmail.com>
2018-06-07 10:08:25 -0400

    fixed opencv3 install take 3

--- scripts/install/opencvInstall.sh
+++ scripts/install/opencvInstall.sh
@@ -24,11 +24,13 @@ make
 sudo make install
 #fix the location of openCV3 if updating an existing system
 cd /usr/local/lib
+echo "shit"
 if [ -d "x86_64-linux-gnu" ]; then
-	cd x86*
-	cp -r * /usr/local/lib
-	cd /usr/local/lib
-	rm -rf x86*
+    echo "shit"
+    cd x86*
+    sudo cp -r * /usr/local/lib
+    cd /usr/local/lib
+    sudo rm -rf x86*
 fi
 sudo ldconfig
 
