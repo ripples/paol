79cb6f2d62f743aa61ed3facb83ab93cad42039a
paol <vasko1996@yahoo.com>
2018-06-12 14:45:11 -0400

    debugging

--- scripts/notification/parseErrors.py
+++ scripts/notification/parseErrors.py
@@ -5,40 +5,6 @@ import signal
 
 path = "/home/paol/recordings/notifyInfoAll.txt"
 
-'''
-Opens a log file at a given path
-Reads through the file and looks for certian error key words
-If found, returns True, if not, returns False
-'''
-'''
-def parseFile():
-	textFile = open(path + "logs/ffmpeg.log", "r")
-	lines = textFile.readlines()
-	x = 0
-	for i in lines:
-	#changes file to be lowercase
-		theLine = lines[x].lower()
-		if "error" in theLine:
-			print("Found")
-			return True
-		if "critical" in theLine:
-			print("Found")
-			return True
-		if "abort" in theLine:
-			print("Found")
-			return True
-		if "core dump" in theLine:
-			print("Found")
-			return True
-	if "warning" in theLine:
-		print("Found")
-		return True
-		x += 1
-
-	textFile.close()
-	return False
-'''
-
 
 def parseInfos():
     message = ''
@@ -52,24 +18,29 @@ def parseInfos():
         for text in info:
             message += text
         infoFile.close()
-        message+= "\n\n"
+        message += "\n\n"
     allInfoFile.close()
     return message
+
 
 def main():
     message = ''
     output = parseInfos()
+    print 'first'
 
     for i in output:
         message += i
     # replaces ' ' with '_' because python hates white space
     message = message.replace(" ", "_")
+    print 'second'
     # command to be sent to shell, takes command line arguements to be sent to emailerror.py
     cmd = "python /home/paol/paol-code/scripts/notification/emailerror.py TEST " + "'" + message + "'"
+    print 'third'
     # sends command to shell
     cmd = shlex.split(cmd)
     process = subprocess.Popen(cmd)
     (result, err) = process.communicate()
+    print 'last'
 
 
 main()
