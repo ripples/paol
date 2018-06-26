9dfad346a4444801461c2b56853efb4de34da0dc
paol <vasko1996@yahoo.com>
2018-06-26 10:50:44 -0400

    fixed installs

--- scripts/install/gstreamerInstall.sh
+++ scripts/install/gstreamerInstall.sh
@@ -10,7 +10,15 @@ sudo apt-get install v4l-utils
 
 #Install gstreamer package and plugins
 
-sudo apt-get install gstreamer1.0*
+sudo apt-get install libgstreamer1.0-0
+sudo apt-get install gstreamer1.0-plugins-base
+sudo apt-get install gstreamer1.0-plugins-good
+sudo apt-get install gstreamer1.0-plugins-bad
+sudo apt-get install gstreamer1.0-plugins-ugly
+sudo apt-get install gstreamer1.0-libav
+sudo apt-get install gstreamer1.0-doc
+sudo apt-get install gstreamer1.0-tools
 sudo apt-get install gstreamer-tools
 sudo apt-get install ubuntu-restricted-extras
+
 
