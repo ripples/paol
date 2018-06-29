ae3b874a301439902952b44058a408901ed99304
paol <vasko1996@yahoo.com>
2018-06-29 15:10:42 -0400

    fixed dependency problem for vga2usb

--- scripts/install/vga2usbPermission.sh
+++ scripts/install/vga2usbPermission.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# install vga2usb driver
+# install vga2usb driver, dkms is needed as dependency 
 # setup vga2usb permissions so that plugging in device will 
 # automatically change permissions on it
 
 cd
 cd Downloads
+sudo apt-get install dkms
 wget http://updates.epiphan.com/downloads/Drivers/Ubuntu/vga2usb-3.30.6.5-ubuntu-4.15.0-22-generic-x86_64-41987-1799.deb
 sudo dpkg -i vga2usb-*
 cd 
